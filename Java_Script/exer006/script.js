6f9aa1902e159e70668641f6ddb2f7b9962ac131
João Filipe <126769503+FilipeDiass@users.noreply.github.com>
2023-06-12 08:03:26 -0300

    att

---
+++ Java_Script/exer006/script.js
@@ -0,0 +1,15 @@
+var button = document.querySelector('#button-inp')
+button.addEventListener('click', clique)
+
+function clique(){
+    var n1 = Number(document.querySelector('#input1').value)
+    var n2 = Number(document.querySelector('#input2').value)
+    var n3 = Number(document.querySelector('#input3').value)
+    var res = document.querySelector('#res')
+
+    var maior = n1 
+    var meio = n1
+    var menor = n1
+
+
+}
